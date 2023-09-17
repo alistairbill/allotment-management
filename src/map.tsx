c7854fd49becfcc5780fd610c846b150c686cf6c
Alistair Bill <alistair.bill@gmail.com>
2023-09-17 18:18:35 +0100

    Adjust wording

--- src/map.tsx
+++ src/map.tsx
@@ -39,7 +39,7 @@ export default function Map() {
                 <div class="p-6">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Plot {selectedName}</h5>
                     <div className="mb-4">
-                        <label for="owner" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Holder</label>
+                        <label for="owner" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Plot holder</label>
                         <input id="owner" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             value={owner} onChange={(e) => setOwner(e.currentTarget.value)} onBlur={updateOwner} />
                     </div>
